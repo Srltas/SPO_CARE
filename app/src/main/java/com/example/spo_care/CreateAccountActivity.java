0fe7eb4985974f74fec811e415c723c6890c248a
Kang-dot <jgbu97@gmail.com>
2020-05-21 00:05:07 +0900

    로그인 형태 수정

---
+++ app/src/main/java/com/example/spo_care/CreateAccountActivity.java
@@ -0,0 +1,82 @@
+package com.example.spo_care;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText editTextEmail;
+    EditText editTextPassword;
+    Button createAccount;
+    TextView returnLogin;
+
+    FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_account);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if(firebaseAuth.getCurrentUser() != null){
+            finish();
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        }
+
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextEmail);
+        createAccount = (Button) findViewById(R.id.btnCreateAccount);
+        returnLogin = (TextView) findViewById(R.id.returnLogin);
+        createAccount.setOnClickListener(this);
+    }
+
+    private void registerUser(){
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this, "email을 입력해 주세요", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this, "password를 입력해 주세요", Toast.LENGTH_SHORT).show();
+        }
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            finish();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            Toast.makeText(CreateAccountActivity.this, "등록에러", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+    @Override
+    public void onClick(View view){
+        if(view == createAccount) {
+            registerUser();
+        }
+
+        if(view == returnLogin){
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+    }
+}
---
+++ app/src/main/java/com/example/spo_care/FindPasswordActivity.java
@@ -0,0 +1,57 @@
+package com.example.spo_care;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class FindPasswordActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText editTextUserEmail;
+    Button btnFindPassword;
+    TextView returnLogin;
+
+    FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.find_password);
+
+        editTextUserEmail = (EditText) findViewById(R.id.editTextEmail);
+        btnFindPassword = (Button) findViewById(R.id.buttonFind);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        btnFindPassword.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view){
+        if(view == btnFindPassword){
+            String emailAddress = editTextUserEmail.getText().toString().trim();
+            firebaseAuth.sendPasswordResetEmail(emailAddress)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(FindPasswordActivity.this, "이메일을 보냈습니다.", Toast.LENGTH_LONG).show();
+                                finish();
+                                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                            } else {
+                                Toast.makeText(FindPasswordActivity.this, "메일 발송 실패", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+        }
+    }
+}
