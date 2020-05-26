109a1962f02377fa795dc91246001f0d1f65dd1c
Dong_Min Kim <kdm97912@gmail.com>
2020-05-27 00:34:17 +0900

    #Min_branch 해결 못하는 오류로 인해서 Min_branch2로 변경 #Min_branch -> Min_branch2로 pull 1. MouthTraining.java mouth_training.xml 화면생성 2. MainActivity.java 9개 아이콘 터치 시 다른 화면으로 넘어가게 설정 3. selftest.xml 디자인 변경 4. selftest_cavity.xml, selftest_periodontl_disease.xml 라디오버튼 텍스트 색 설정 5. drawalbe안에 있는 png, xml 파일명 정리

--- app/src/main/java/com/example/spo_care/MainActivity.java
+++ app/src/main/java/com/example/spo_care/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.spo_care;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
 
-public class MainActivity extends Activity implements View.OnClickListener{
+public class MainActivity extends Activity {
 
     ImageView btnMyPage, btnSelfTest, btnTeethCareItem, btnVisualImpairment,
             btnBrainLesions, btnHearingImpairment, btnDisablePeopleDentistry,
@@ -17,57 +18,68 @@ public class MainActivity extends Activity implements View.OnClickListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnMyPage = (ImageView) findViewById(R.id.btnMyPage);
-        btnSelfTest = (ImageView) findViewById(R.id.btnSelfTest);
-        btnTeethCareItem = (ImageView) findViewById(R.id.btnTeethCareItem);
-        btnVisualImpairment = (ImageView) findViewById(R.id.btnVisualImpairment);
-        btnTeethBrushTimer = (ImageView) findViewById(R.id.btnTeethBrushTimer);
-        btnBrainLesions = (ImageView) findViewById(R.id.btnBrainLesions);
-        btnHearingImpairment = (ImageView) findViewById(R.id.btnHearingImpairment);
-        btnDisablePeopleDentistry = (ImageView) findViewById(R.id.btnDisablePeopleDentistry);
-        btnMouthTraining = (ImageView) findViewById(R.id.btnMouthTraining);
+        btnMyPage = findViewById(R.id.btnMyPage);
+        btnSelfTest = findViewById(R.id.btnSelfTest);
+        btnTeethCareItem = findViewById(R.id.btnTeethCareItem);
+        btnVisualImpairment = findViewById(R.id.btnVisualImpairment);
+        btnTeethBrushTimer = findViewById(R.id.btnTeethBrushTimer);
+        btnBrainLesions = findViewById(R.id.btnBrainLesions);
+        btnHearingImpairment = findViewById(R.id.btnHearingImpairment);
+        btnDisablePeopleDentistry = findViewById(R.id.btnDisablePeopleDentistry);
+        btnMouthTraining = findViewById(R.id.btnMouthTraining);
 
-        btnTeethCareItem.setOnClickListener(this);
-        btnSelfTest.setOnClickListener(this);
-        btnTeethCareItem.setOnClickListener(this);
-        btnVisualImpairment.setOnClickListener(this);
-        btnTeethBrushTimer.setOnClickListener(this);
-        btnBrainLesions.setOnClickListener(this);
-        btnHearingImpairment.setOnClickListener(this);
-        btnDisablePeopleDentistry.setOnClickListener(this);
-        btnMouthTraining.setOnClickListener(this);
-
-    }
-
-    @Override
-    public void onClick(View view){
-        if(view == btnMyPage){
-            //아직 없음
-        }
-        if(view == btnSelfTest){
-            //startActivity(new Intent(this, selfTestActivity.class));
-        }
-        if(view == btnTeethCareItem){
-
-        }
-        if(view == btnVisualImpairment){
-
-        }
-        if(view == btnTeethBrushTimer){
-
-        }
-        if(view == btnBrainLesions){
-
-        }
-        if(view == btnHearingImpairment){
-
-        }
-        if(view == btnDisablePeopleDentistry){
-
-        }
-        if(view == btnMouthTraining){
-
-        }
-
+        View.OnClickListener listener = new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                switch (view.getId()) {
+                    case R.id.btnMyPage:
+                        Intent myPageIntent = new Intent(getApplicationContext(), MyPageActivity.class);
+                        startActivity(myPageIntent);
+                        break;
+                    case R.id.btnSelfTest:
+                        Intent selfTestIntent = new Intent(getApplicationContext(), SelfTestActivity.class);
+                        startActivity(selfTestIntent);
+                        break;
+                    case R.id.btnTeethCareItem:
+                        Intent teethCareItemIntent = new Intent(getApplicationContext(), TeethCareItemActivity.class);
+                        startActivity(teethCareItemIntent);
+                        break;
+                    case R.id.btnVisualImpairment:
+                        Intent visualImpairmentIntent = new Intent(getApplicationContext(), VisualImpairmentActivity.class);
+                        startActivity(visualImpairmentIntent);
+                        break;
+                    case R.id.btnTeethBrushTimer:
+                        Intent teethBrushTimerIntent = new Intent(getApplicationContext(), TeethBrushTimerActivity.class);
+                        startActivity(teethBrushTimerIntent);
+                        break;
+                    case R.id.btnBrainLesions:
+                        Intent brainLesionsIntent = new Intent(getApplicationContext(), BrainLesionsActivity.class);
+                        startActivity(brainLesionsIntent);
+                        break;
+                    case R.id.btnHearingImpairment:
+                        Intent hearingImpairmentIntent = new Intent(getApplicationContext(), HearingImpairmentActivity.class);
+                        startActivity(hearingImpairmentIntent);
+                        break;
+                    case R.id.btnDisablePeopleDentistry:
+                        Intent disablePeopleDentistryIntent = new Intent(getApplicationContext(), DisablePeopleDentistry.class);
+                        startActivity(disablePeopleDentistryIntent);
+                        break;
+                    case R.id.btnMouthTraining:
+                        Intent mouthTrainingIntent = new Intent(getApplicationContext(), MouthTraining.class);
+                        startActivity(mouthTrainingIntent);
+                        break;
+                }
+            }
+        };
+        btnMyPage.setOnClickListener(listener);
+        btnTeethCareItem.setOnClickListener(listener);
+        btnSelfTest.setOnClickListener(listener);
+        btnTeethCareItem.setOnClickListener(listener);
+        btnVisualImpairment.setOnClickListener(listener);
+        btnTeethBrushTimer.setOnClickListener(listener);
+        btnBrainLesions.setOnClickListener(listener);
+        btnHearingImpairment.setOnClickListener(listener);
+        btnDisablePeopleDentistry.setOnClickListener(listener);
+        btnMouthTraining.setOnClickListener(listener);
     }
 }
