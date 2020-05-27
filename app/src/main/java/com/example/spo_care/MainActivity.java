0b926eec85c8cced6778ef86837146eea9253a08
Dong_Min Kim <kdm97912@gmail.com>
2020-05-27 23:52:29 +0900

    MainActivity.java - 뒤로가기 2초 안에 2번 누를 시 앱 종료 추가

--- app/src/main/java/com/example/spo_care/MainActivity.java
+++ app/src/main/java/com/example/spo_care/MainActivity.java
@@ -5,9 +5,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
+
+    private final long FINISH_INTERVAL_TIME = 2000;
+    private long backPressedTime = 0;
 
     ImageView btnMyPage, btnSelfTest, btnTeethCareItem, btnVisualImpairment,
             btnBrainLesions, btnHearingImpairment, btnDisablePeopleDentistry,
@@ -81,5 +85,19 @@ public class MainActivity extends Activity {
         btnHearingImpairment.setOnClickListener(listener);
         btnDisablePeopleDentistry.setOnClickListener(listener);
         btnMouthTraining.setOnClickListener(listener);
+    }
+
+    //뒤로가기 2번 누를 시 종료
+    @Override
+    public void onBackPressed(){
+        long tempTime = System.currentTimeMillis();
+        long intervalTime = tempTime - backPressedTime;
+
+        if(0 <= intervalTime && FINISH_INTERVAL_TIME >= intervalTime){
+            super.onBackPressed();
+        } else{
+            backPressedTime = tempTime;
+            Toast.makeText(getApplicationContext(), "한번 더 뒤로가기를 누르면 종료됩니다.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
