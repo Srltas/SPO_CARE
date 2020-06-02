54beb494554a57b1363cd7e0a07e11fd1487286e
Dong_Min Kim <kdm97912@gmail.com>
2020-06-02 16:41:41 +0900

    유튜브 동영상 재상 기능 YouTubeAndroidPlayerApi.jar 추가

--- app/src/main/java/com/example/spo_care/MouthTraining.java
+++ app/src/main/java/com/example/spo_care/MouthTraining.java
@@ -1,12 +1,28 @@
 package com.example.spo_care;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MouthTraining extends Activity {
+
+    ImageButton btn1;
+
     @Override
     protected void onCreate(Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
         setContentView(R.layout.mouth_training);
+
+        btn1 = findViewById(R.id.thumbnail_1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent VideoViewIntent = new Intent(getApplicationContext(), VideoViewController.class);
+                startActivity(VideoViewIntent);
+            }
+        });
     }
 }
