bd930fab881844272934469352884484cd78c8c5
Dong_Min Kim <kdm97912@gmail.com>
2020-06-02 22:55:37 +0900

    유튜브 재생 기능 / 장애인 치과병원 지도 xml 디자인 MouthTraining.java, VideoViewController.java  -메소드 추가 및 버튼 아이디 연결 -------------------------------------------------------------- map_view.xml - 전체 디자인

--- app/src/main/java/com/example/spo_care/VideoViewController.java
+++ app/src/main/java/com/example/spo_care/VideoViewController.java
@@ -1,16 +1,21 @@
 package com.example.spo_care;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
 
 
-public class VideoViewController extends YouTubeBaseActivity implements View.OnClickListener{
+public class VideoViewController extends YouTubeBaseActivity {
 
+    private final String API_KEY = "AIzaSyCO67ZZRKUzc7kwNQ3LqD7hlRl0T2IG2_M";
+    private final String VIDEO_ID_1 = "uR8Mrt1IpXg";
+    private final String VIDEO_ID_2 = "Raj-AuQgCTg";
+    private final String VIDEO_ID_3 = "ALZHF5UqnU4";
+    private final String VIDEO_ID_4 = "1usq4sVWWls";
+    private String message;
     YouTubePlayerView youtubeView;
     YouTubePlayer.OnInitializedListener listener;
 
@@ -20,10 +25,14 @@ public class VideoViewController extends YouTubeBaseActivity implements View.OnC
         setContentView(R.layout.video_view);
 
         youtubeView = (YouTubePlayerView) findViewById(R.id.youtubeView);
+
+        Intent videoViewController = getIntent();
+        message = videoViewController.getStringExtra("video");
+
         listener = new YouTubePlayer.OnInitializedListener() {
             @Override
             public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-                youTubePlayer.loadVideo("uR8Mrt1IpXg");
+                youTubePlayer.loadVideo(selectVideo(message),YouTubePlayer.FULLSCREEN_FLAG_CUSTOM_LAYOUT);
             }
 
             @Override
@@ -31,11 +40,23 @@ public class VideoViewController extends YouTubeBaseActivity implements View.OnC
 
             }
         };
+        startYouTube();
     }
-    @Override
-    public void onClick(View view){
-        if(view == youtubeView){
-            youtubeView.initialize("AIzaSyCO67ZZRKUzc7kwNQ3LqD7hlRl0T2IG2_M",listener);
+    public void startYouTube(){
+        youtubeView.initialize(API_KEY,listener);
+    }
+
+    public String selectVideo(String message){
+        switch(message){
+            case "mouthTrainingVideo_1":
+                return VIDEO_ID_1;
+            case "mouthTrainingVideo_2":
+                return VIDEO_ID_2;
+            case "mouthTrainingVideo_3":
+                return VIDEO_ID_3;
+            case "mouthTrainingVideo_4":
+                return VIDEO_ID_4;
         }
+        return null;
     }
 }
