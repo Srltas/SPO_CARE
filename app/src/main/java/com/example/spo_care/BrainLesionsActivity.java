33116a6ba8228976cd9a875cf0de0a2b092d1903
Dong_Min Kim <kdm97912@gmail.com>
2020-06-05 18:30:46 +0900

    각 장애별 파트 음성서비스 추가 / 앱 아이콘 변경 / 메인화면 비율 조정

--- app/src/main/java/com/example/spo_care/BrainLesionsActivity.java
+++ app/src/main/java/com/example/spo_care/BrainLesionsActivity.java
@@ -6,21 +6,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 public class BrainLesionsActivity extends Activity implements View.OnClickListener {
 
-    private ImageView characteristic;
+    private ImageView characteristic, precautions, tip, visit;
     private MediaPlayer mediaPlayer;
 
-    private boolean checkMedia = true;
     @Override
     protected void onCreate(Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
         setContentView(R.layout.brain_lesions);
 
-        characteristic = findViewById(R.id.icon_brain_lesions_characteristic);
+        characteristic = findViewById(R.id.speak_brain_lesions_characteristic);
+        precautions = findViewById(R.id.speak_brain_lesions_precautions);
+        tip = findViewById(R.id.speak_brain_lesions_tip);
+        visit = findViewById(R.id.speak_brain_lesions_visit);
 
+        visit.setOnClickListener(this);
+        tip.setOnClickListener(this);
+        precautions.setOnClickListener(this);
         characteristic.setOnClickListener(this);
     }
 
@@ -28,23 +31,28 @@ public class BrainLesionsActivity extends Activity implements View.OnClickListen
     @Override
     public void onClick(View view) {
         switch (view.getId()){
-            case R.id.icon_brain_lesions_characteristic:
+            case R.id.speak_brain_lesions_characteristic:
                 playMedia(R.raw.sound_brain_lesions_characteristic);
+                break;
+            case R.id.speak_brain_lesions_precautions:
+                playMedia(R.raw.sound_brain_lesions_precautions);
+                break;
+            case R.id.speak_brain_lesions_tip:
+                playMedia(R.raw.sound_brain_lesions_tip);
+                break;
+            case R.id.speak_brain_lesions_visit:
+                playMedia(R.raw.sound_brain_lesions_visit);
+                break;
         }
     }
 
     public void playMedia(int id){
-        if(checkMedia){
-            checkMedia = false;
-            mediaPlayer = MediaPlayer.create(BrainLesionsActivity.this,id);
-            mediaPlayer.start();
-        }else{
-            checkMedia = true;
-            if(mediaPlayer.isPlaying()){
-                mediaPlayer.stop();
-                mediaPlayer.reset();
-            }
+        if(mediaPlayer != null){
+            mediaPlayer.stop();
+            mediaPlayer.reset();
         }
+        mediaPlayer = MediaPlayer.create(BrainLesionsActivity.this,id);
+        mediaPlayer.start();
     }
 
     @Override
